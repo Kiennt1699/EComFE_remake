6c23f661cb7b9f97e226efc6b6c5ad49d728cd5e
84702 <84702@DONGLOI>
2024-10-14 09:32:49 +0700

    LandingPage

--- AndroidStudioProjects/EComFE/app/build.gradle.kts
+++ AndroidStudioProjects/EComFE/app/build.gradle.kts
@@ -1,13 +1,15 @@
+import com.android.build.api.dsl.ViewBinding
+
 plugins {
     alias(libs.plugins.android.application)
 }
 
 android {
-    namespace = "com.example.ecomfe"
+    namespace = "com.example.myapplication"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.ecomfe"
+        applicationId = "com.example.myapplication"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -29,7 +31,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    buildFeatures {
+    buildFeatures{
         viewBinding = true
     }
 }
@@ -38,10 +40,10 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.navigation.fragment)
-    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
--- AndroidStudioProjects/EComFE/settings.gradle.kts
+++ AndroidStudioProjects/EComFE/settings.gradle.kts
@@ -19,6 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "EComFE"
+rootProject.name = "My Application"
 include(":app")
  

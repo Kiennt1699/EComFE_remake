cfd6c25ccfcd426e6ce894c4033c8ad03b83fdc1
Reinr <blackwhitenxt@gmail.com>
2024-11-03 16:00:45 +0700

    fix bug

--- AndroidStudioProjects/EComFE/app/src/main/java/com/example/myapplication/Activity/UserSettingsActivity.java
+++ AndroidStudioProjects/EComFE/app/src/main/java/com/example/myapplication/Activity/UserSettingsActivity.java
@@ -16,28 +16,27 @@ import Adapter.UserPageAdapter;
 import Domain.GroupObject;
 import Domain.ItemObject;
 
-public class UserSettingsActivity extends AppCompatActivity {
+public class UserSettingsActivity  {
 
     ExpandableListView expandableListView;
     UserPageAdapter adapter;
     List<GroupObject> listGroup;
     HashMap<GroupObject, List<ItemObject>> listItem;
+    AppCompatActivity context;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_page);
-
-        expandableListView = findViewById(R.id.expandableListView);
+    public UserSettingsActivity(AppCompatActivity context) {
+        this.context = context;
+        expandableListView = context.findViewById(R.id.expandableListView);
         listGroup = new ArrayList<>();
         listItem = new HashMap<>();
 
-        // Khởi tạo dữ liệu
-        initData(); // Đảm bảo bạn đã gọi initData trước khi tạo adapter
+
 
         // Tạo adapter
-        adapter = new UserPageAdapter(this, listGroup, listItem);
+        adapter = new UserPageAdapter(context, listGroup, listItem);
         expandableListView.setAdapter(adapter);
+        // Khởi tạo dữ liệu
+        initData(); // Đảm bảo bạn đã gọi initData trước khi tạo adapter
     }
 
     // Hàm initData()
@@ -58,44 +57,25 @@ public class UserSettingsActivity extends AppCompatActivity {
         Log.d("UserSettingsActivity", "Group added: " + wishGroup.getName());
 
         listGroup.add(cartGroup);
-        Log.d("UserSettingsActivity", "Group added: " + cartGroup.getName());
-
         listGroup.add(userGroup);
-        Log.d("UserSettingsActivity", "Group added: " + userGroup.getName());
-
         listGroup.add(historyGroup);
-        Log.d("UserSettingsActivity", "Group added: " + historyGroup.getName());
 
-        // Khởi tạo các mục con cho từng nhóm
-        List<ItemObject> homeItems = new ArrayList<>();
-        homeItems.add(new ItemObject(1, "Dashboard"));
-        homeItems.add(new ItemObject(2, "Notifications"));
 
-        // In ra log để kiểm tra mục con
-        Log.d("UserSettingsActivity", "Adding items to group: " + homeGroup.getName());
-        for (ItemObject item : homeItems) {
-            Log.d("UserSettingsActivity", "Item added: " + item.getName());
-        }
 
-        listItem.put(homeGroup, homeItems);
+
 
         // Các mục con cho các nhóm khác
+        listItem.put(homeGroup, new ArrayList<>()); // Không có mục cho wishlist
         listItem.put(wishGroup, new ArrayList<>()); // Không có mục cho wishlist
         listItem.put(cartGroup, new ArrayList<>()); // Không có mục cho cart
         listItem.put(userGroup, new ArrayList<>()); // Không có mục cho user profile
         listItem.put(historyGroup, new ArrayList<>()); // Không có mục cho history cart
 
-        // In ra log để kiểm tra trạng thái của listGroup và listItem
-        Log.d("UserSettingsActivity", "Group count: " + listGroup.size());
         for (GroupObject group : listGroup) {
-            Log.d("UserSettingsActivity", "Group: " + group.getName());
             List<ItemObject> items = listItem.get(group);
-            Log.d("UserSettingsActivity", "Items count in group " + group.getName() + ": " + (items != null ? items.size() : 0));
         }
 
         // Thông báo cho adapter về thay đổi dữ liệu
         adapter.notifyDataSetChanged();
     }
-
-
 }
